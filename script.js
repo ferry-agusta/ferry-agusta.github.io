55f37cfff9d478d12b190f11ecf0b4249529b385
ferry-agusta <ferry.agusta.wk@gmail.com>
2024-11-28 09:32:09 +0700

    cari

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+// Feather Icon
 feather.replace();
+
+
+// Dialog-Modal
+const modal = document.querySelector("[data-modal]")
+const openButton = document.querySelector("[data-open-modal]")
+// const closeButton = document.querySelector("[data-close-modal]")
+
+openButton.addEventListener("click", () => {
+  modal.showModal();
+})
+
+// closeButton.addEventListener("click", () => {
+//   modal.close();
+// })
+
+modal.addEventListener("click", e => {
+  const dialogDimensions = modal.getBoundingClientRect()
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    modal.close()
+  }
+})
