daca89140b4098d3694709d60257c7608e1b0746
ferry-agusta <ferry.agusta.wk@gmail.com>
2024-12-25 13:12:04 +0700

    css js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
 // Feather Icon
 feather.replace();
 
+// SWIPERJS
+var swiperThumbs = new Swiper(".mySwiperThumbs", {
+  spaceBetween: 2,
+  slidesPerView: 8,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiperGalery", {
+  spaceBetween: 0,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination-galery",
+    clickable: true,
+  },
+  thumbs: {
+    swiper: swiperThumbs,
+  },
+});
+
 // Swiper JS
 var heroSwiper = new Swiper(".swiper1", {
   slidesPerView: 2,
@@ -95,34 +118,34 @@ modalCariDown.addEventListener("click", e => {
 
 
 // OVERLAY
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  // captionText.innerHTML = dots[slideIndex - 1].alt;
-}
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("demo");
+//   let captionText = document.getElementById("caption");
+//   if (n > slides.length) { slideIndex = 1 }
+//   if (n < 1) { slideIndex = slides.length }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+//   // captionText.innerHTML = dots[slideIndex - 1].alt;
+// }
 
 
 
